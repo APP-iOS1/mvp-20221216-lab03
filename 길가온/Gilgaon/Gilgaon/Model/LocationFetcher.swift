68820db78e4a13d660c70e56f7006dd8bd097044
stealmh <kmh922@naver.com>
2023-03-16 22:32:35 +0900

    [Feat] 동선 Create, Read 함수 구현

--- 길가온/Gilgaon/Gilgaon/Model/LocationFetcher.swift
+++ 길가온/Gilgaon/Gilgaon/Model/LocationFetcher.swift
@@ -1,6 +1,10 @@
 import CoreLocation
 import SwiftUI
 import MapKit
+import Firebase
+import FirebaseAuth
+import Combine
+
 
 class LocationFetcher: NSObject, CLLocationManagerDelegate,ObservableObject {
     let manager = CLLocationManager()
@@ -13,6 +17,12 @@ class LocationFetcher: NSObject, CLLocationManagerDelegate,ObservableObject {
 //        CLLocationCoordinate2D(latitude: 37.251384139941024, longitude: 127.06349117923736),
     ]
     
+    let testPoint: [[CLLocationCoordinate2D]] = [
+        [CLLocationCoordinate2D(latitude: 37.25062407449622, longitude: 127.0635877387619),CLLocationCoordinate2D(latitude: 37.25111939891473, longitude: 127.0639310615158),CLLocationCoordinate2D(latitude: 37.251384139941024, longitude: 127.06349117923736)],        [CLLocationCoordinate2D(latitude: 36.25062407449622, longitude: 126.0635877387619),CLLocationCoordinate2D(latitude: 36.25111939891473, longitude: 126.0639310615158),CLLocationCoordinate2D(latitude: 36.251384139941024, longitude: 126.06349117923736)]
+    ]
+    
+    var cancellable = Set<AnyCancellable>()
+    let touchCell = CurrentValueSubject<[CLLocationCoordinate2D],Never>([])
 
     override init() {
         super.init()
@@ -80,6 +90,50 @@ class LocationFetcher: NSObject, CLLocationManagerDelegate,ObservableObject {
     }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("error")
+    }
+    
+    
+    func stopLocation(_ scheduleData: MarkerModel) {
+        //1. 위치정보를 끈다.
+        manager.stopUpdatingLocation()
+        //2. 배열을 서버로 전송한다.
+        Firestore.firestore()
+            .collection("User")
+            .document(Auth.auth().currentUser?.uid ?? "")
+            .collection("Calendar")
+            .document(scheduleData.id)
+            .collection("TrafficLine")
+            .document("dongseon")
+            .setData(["myTrafficLine":points])
+    }
+    
+    func getTrafficLine(_ scheduleData: MarkerModel) async{
+        let ref = Firestore
+                        .firestore()
+                        .collection("User")
+                        .document(Auth.auth().currentUser?.uid ?? "")
+                        .collection("Calendar")
+                        .document(scheduleData.id)
+                        .collection("TrafficLine")
+                        .document("dongseon")
+        
+        do {
+            let snapShot = try await ref.getDocument()
+            let docData = snapShot.data()!
+            let data = docData["myTrafficLine"] as? [CLLocationCoordinate2D] ?? []
+            touchCell.send(data)
+        } catch {
+            print("동선데이터를 받아올 수 없음")
+        }
+    }
+    
+    func a() {
+        touchCell
+            .receive(on: DispatchQueue.main)
+//            .assign(to: &sad)
+            .sink { trafficLine in
+                print(trafficLine)
+            }.store(in: &cancellable)
     }
     
 }
