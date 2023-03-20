9bcf9b22df246efbc41649e8bbf885328ad25298
JIN-JJS <joonsu2005@naver.com>
2023-03-20 12:23:26 +0900

    [Add] FriendGuestBookView, FriendGuestBookWritingView #59

---
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/FriendGuestBookView.swift
@@ -0,0 +1,20 @@
+//
+//  FriendGuestBookView.swift
+//  Gilgaon
+//
+//  Created by 전준수 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct FriendGuestBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendGuestBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendGuestBookView()
+    }
+}
