23974f7689eeb715c99c18d50399b9325ef047aa
jeongsoohee <dkssudgo365@naver.com>
2023-03-21 13:31:57 +0900

    [Chore] Combine적용한 변수와 함수로 변경  #57

--- 길가온/Gilgaon/Gilgaon/View/TestAPIView.swift
+++ 길가온/Gilgaon/Gilgaon/View/TestAPIView.swift
@@ -14,94 +14,82 @@ struct TestAPIView: View {
     @EnvironmentObject var viewModel: SearchViewModel
     @ObservedObject var jogakData: JogakData = JogakData()
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var searchText: String = ""
+    //    @State private var searchText: String = ""
+    @State private var isSearching: Bool =  false
     @Binding var lonString: String
     @Binding var lanString: String
     @Binding var locationName: String
     //    @EnvironmentObject private var firestore: FireStoreViewModel
     
     
-//    @State private var testFilter: [Poi] = []
+    //    @State private var testFilter: [Poi] = []
     
     
-//    var filterData: [Poi] {
-//        if searchText.isEmpty {
-//            return viewModel.center!.searchPoiInfo.pois.poi
-//        } else {
-//            return viewModel.center!.searchPoiInfo.pois.poi.filter
-//            {$0.name.localizedStandardContains(searchText)}
-//        }
-//    }
+    //    var filterData: [Poi] {
+    //        if searchText.isEmpty {
+    //            return viewModel.center!.searchPoiInfo.pois.poi
+    //        } else {
+    //            return viewModel.center!.searchPoiInfo.pois.poi.filter
+    //            {$0.name.localizedStandardContains(searchText)}
+    //        }
+    //    }
     
-//    var getUser: [FireStoreModel] {
-//        Task {
-//            try! await firestore.searchUser(searchText)
-//        }
-//        return firestore.users.filter {$0.nickName.localizedStandardContains(searchText)}
-//    }
-
+    //    var getUser: [FireStoreModel] {
+    //        Task {
+    //            try! await firestore.searchUser(searchText)
+    //        }
+    //        return firestore.users.filter {$0.nickName.localizedStandardContains(searchText)}
+    //    }
     
     
-//    var filterData: [Poi] {
-//        Task {
-//            viewModel.center = try await searchNetwork.loadJson(searchTerm: searchText)
-//        }
-//            return viewModel.center!.searchPoiInfo.pois.poi.filter {$0.name.localizedStandardContains(searchText)}
-//
-//
-//    }
-
+    
+    //    var filterData: [Poi] {
+    //        Task {
+    //            viewModel.center = try await searchNetwork.loadJson(searchTerm: searchText)
+    //        }
+    //            return viewModel.center!.searchPoiInfo.pois.poi.filter {$0.name.localizedStandardContains(searchText)}
+    //
+    //
+    //    }
+    
     
     var body: some View {
         NavigationView {
             VStack {
-//                if viewModel.center?.searchPoiInfo.pois.poi.count ?? 0 > 0 {
-                HStack {
-                    TextField("장소 검색", text: $searchNetwork.searchText)
-                    Button("검색") {
-                        searchNetwork.searchResult()
+                //                if viewModel.center?.searchPoiInfo.pois.poi.count ?? 0 > 0 {
+                SearchBarView(searchNetwork: searchNetwork, isSearching: $isSearching)
+                
+                List(searchNetwork.searchResultArray ,id:\.self) { datum in
+                    //                        Text("\(datum.name)")
+                    Button {
+                        locationName = datum.name //목적지의 이름
+                        lonString = datum.frontLon //목적지의 이름
+                        lanString = datum.frontLat // 목적지의 경도
+                        
+                        dismiss()
+                        vm.doSomeThing()
+                    } label: {
+                        Text("\(datum.name)")
+                            .font(.custom("NotoSerifKR-SemiBold", size: 15))
                     }
                 }
-
-                    List(searchNetwork.searchResultArray ,id:\.self) { datum in
-    //                        Text("\(datum.name)")
-                            Button {
-                                locationName = datum.name //목적지의 이름
-                                lonString = datum.frontLon //목적지의 이름
-                                lanString = datum.frontLat // 목적지의 경도
-                                
-                                
-    //                            LocationsDataService.locations.append(
-    //                                Location(
-    //                                    name: datum.name,
-    //                                    cityName: "경기 안산시 단원구 원곡동 906",
-    //                                    coordinate: CLLocationCoordinate2D(latitude: Double(datum.frontLat)!, longitude: Double(datum.frontLon)!),
-    //                                    description: "...",
-    //                                    imageNames: ["b00"],
-    //                                    link: "dfsdfsdf"))
-    //                            jogakData.datum.append(datum)
-                                dismiss()
-                                vm.doSomeThing()
-                            } label: {
-                                Text("\(datum.name)")
-                                    .font(.custom("NotoSerifKR-SemiBold", size: 15))
-                            }
-                    }
+                .listStyle(.plain)
                 
             }
+            .onAppear {
+                searchNetwork.searchResult()
+            }
         }
-        .onAppear {
-            searchNetwork.searchResult()
-        }
-//        .onAppear {
-//            Task {viewModel.center = try await searchNetwork.loadJson(searchTerm: "초지역")
-//            }
-////            print(viewModel.center?.searchPoiInfo.pois.poi.count)
-//
-//
-//        }
+        
+        //        .onAppear {
+        //            Task {viewModel.center = try await searchNetwork.loadJson(searchTerm: "초지역")
+        //            }
+        ////            print(viewModel.center?.searchPoiInfo.pois.poi.count)
+        //
+        //
+        //        }
+        
     }
-
 }
 
 struct TestAPIView_Previews: PreviewProvider {
