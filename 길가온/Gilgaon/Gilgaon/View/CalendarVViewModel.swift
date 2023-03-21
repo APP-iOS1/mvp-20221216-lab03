87e60debd5eac2f7e70af88af381b1c7c2401ba1
Sehoon Jeong <wtd120120@gmail.com>
2023-03-21 15:04:18 +0900

    [Feat] 기록중인 다이어리 ID AppStorage에 저장 및 불러오기 기능 구현 #58

--- 길가온/Gilgaon/Gilgaon/View/CalendarVViewModel.swift
+++ 길가온/Gilgaon/Gilgaon/View/CalendarVViewModel.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 final class CalendarVViewModel: ObservableObject{
     @AppStorage("isRecording") var isRecordingStatus: Bool = UserDefaults.standard.bool(forKey: "isRecording")
+    @AppStorage("recordingKey") var recordingKey: String = UserDefaults.standard.string(forKey: "recordingKey") ?? ""
     @Published var isTapped: Bool = false
     @Published var isRecording: Bool = false
     init(){
-        NotificationCenter.default.addObserver(forName: Notification.Name("isRecording"), object: nil, queue: nil) { _ in
-            self.isRecordingStatus = true
-            self.isRecording = true
-            print(self.isRecordingStatus)
+        NotificationCenter.default.addObserver(forName: Notification.Name("isRecording"), object: nil, queue: nil) { noti in
+            if let (recordingReciv, rKey) = noti.object as? (Bool, String) {
+                self.isRecordingStatus = recordingReciv
+                self.recordingKey = rKey
+            } else{
+                self.isRecordingStatus = false
+                self.isRecording = false
+                self.recordingKey = ""
+            }
         }
-        print("fff")
-    }
-    func t (){
-        NotificationCenter.default.post(name: Notification.Name("isRecording"), object: nil)
-        
     }
 }
