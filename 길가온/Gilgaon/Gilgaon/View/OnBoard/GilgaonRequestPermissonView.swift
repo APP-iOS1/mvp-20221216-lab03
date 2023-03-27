f7b16d977fdd8ed2340f0834175b2119766fb912
JIN-JJS <joonsu2005@naver.com>
2023-03-27 18:11:22 +0900

    [Feat] 온보딩뷰 텍스트 수정 #67

--- 길가온/Gilgaon/Gilgaon/View/OnBoard/GilgaonRequestPermissonView.swift
+++ 길가온/Gilgaon/Gilgaon/View/OnBoard/GilgaonRequestPermissonView.swift
@@ -33,31 +33,42 @@ struct GilgaonRequestPermissonView: View {
                     Text("길가온")
                         .font(.custom("NotoserifKR-Regular", size: 28))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("Pink")) + Text("을 이용하려면 다음이 필요해요.")
+                        .foregroundColor(Color("Pink")) + Text("과 함께 시작해요!")
                         .font(.custom("NotoSerifKR-Regular", size: 28))
                         .foregroundColor(Color("DarkGray"))
                     
-                    VStack(spacing:25){
+                    VStack(alignment: .leading, spacing:25){
                         HStack(alignment: .center, spacing: 16.0) {
                             Image(systemName: "location.fill.viewfinder")
                                 .resizable()
                                 .aspectRatio(1.0, contentMode: .fit)
                                 .frame(width: 30, height: 30)
-                            Text("소중한 하루의 발 자취를 기록할 **위치 접근**을 허용해 주세요.")
-                                .font(.custom("NotoSerifKR-Regular", size: 17))
+                            VStack(alignment: .leading) {
+                                Text("소중한 하루의 발 자취를 기록할")
+                                    .font(.custom("NotoSerifKR-Regular", size: 17))
+                                Text("**위치 접근**을 허용해 주세요.")
+                                    .font(.custom("NotoSerifKR-Regular", size: 17))
+                            }
                         }
                         .foregroundColor(Color("DarkGray"))
+                        
+                       
                         
                         HStack(alignment: .center, spacing: 16.0) {
                             Image(systemName: "camera.viewfinder")
                                 .resizable()
                                 .aspectRatio(1.0, contentMode: .fit)
                                 .frame(width: 30, height: 30)
-                            Text("추억을 남길 사진촬영을 위해 **카메라 접근**을 허용해 주세요.")
-                                .font(.custom("NotoSerifKR-Regular", size: 17))
+                            VStack(alignment: .leading) {
+                                Text("추억을 남길 사진촬영을 위해")
+                                    .font(.custom("NotoSerifKR-Regular", size: 17))
+                                Text("**카메라 접근**을 허용해 주세요.")
+                                    .font(.custom("NotoSerifKR-Regular", size: 17))
+                            }
                         }
                         .foregroundColor(Color("DarkGray"))
                     }
+                    
                     Text("설정하신 모든 접근들은 환경설정에서 변경하실 수 있어요.")
                         .font(.custom("NotoSerifKR-Regular", size: 15))
                         .foregroundColor(Color("DarkGray"))
